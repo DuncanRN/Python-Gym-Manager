48e7e018c86a2e2fe9590bbb56fd8d0f196baa40
DuncanRN <knacknud@gmail.com>
2022-09-24 17:06:10 +0100

    completed app.py, running flask, getting TemplateNotFound error, but that's good! I've not put the index.html file in the right place yet

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 from flask import Flask, render_template
 
+from controllers.member_controller import members_blueprint
+# from controllers.gym_class_controller import gym_classes_blueprint
+# from controllers.booking_controller import bookings_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(members_blueprint)
+# app.register_blueprint(gym_class_blueprint)
+# app.register_blueprint(booking_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
