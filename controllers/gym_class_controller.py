7397c1d7047c9904a427e11c417943a52b382dcf
DuncanRN <knacknud@gmail.com>
2022-09-25 06:18:59 +0100

    successfully adding a new gym_class

--- controllers/gym_class_controller.py
+++ controllers/gym_class_controller.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template
-# , request, redirect
+from flask import Flask, render_template, request, redirect
 from flask import Blueprint
-# from models.gym_class import GymClass
+from models.gym_class import GymClass
 import repositories.gym_class_repository as gym_class_repository
 import repositories.member_repository as member_repository
 
@@ -23,27 +22,18 @@ def show(id):
     all_members = member_repository.select_all()
     return render_template("classes/show.html", gym_class=gym_class, members=members, all_members=all_members)
 
+@gym_classes_blueprint.route("/classes/new")
+def new_gym_class():
+    return render_template("classes/add.html")
 
 
 
-
-
-# from flask import Flask, render_template
-# # , request, redirect
-# from flask import Blueprint
-# # from models.gym_class import GymClass
-# import repositories.gym_class_repository as gym_class_repository
-
-# gym_class_blueprint = Blueprint("gym_class", __name__)
-
-# @gym_class_blueprint.route("/classes")
-# def gym_classes():
-#     print("HERE")
-#     gym_classes = gym_class_repository.select_all() 
-#     return render_template("classes/index.html", classes = gym_classes)
-
-# # @members_blueprint.route("/members/<id>")
-# # def show(id):
-# #     member = member_repository.select(id)
-# #     gym_classes = member_repository.gym_classes(id)
-# #     return render_template("members/show.html", member=member, gym_classes=gym_classes)
+# CREATE
+# POST '/classes'
+@gym_classes_blueprint.route("/classes",  methods=['POST'])
+def create_gym_class():
+    name = request.form['name']
+    
+    gym_class = GymClass(name)
+    gym_class_repository.save(gym_class)
+    return redirect('/classes')
